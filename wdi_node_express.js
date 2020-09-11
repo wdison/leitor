dd83e681e4510165ed6e4b7419001d66c41f2afb
wdison5 <wdison5@gmail.com>
2020-09-11 17:49:33 -0300

    atualizando porta do node

--- wdi_node_express.js
+++ wdi_node_express.js
@@ -3,6 +3,8 @@ var app = express();
 
 app.use(express.static(__dirname));
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, function () {
+  console.log('Example app listening on port '+PORT);
 });
